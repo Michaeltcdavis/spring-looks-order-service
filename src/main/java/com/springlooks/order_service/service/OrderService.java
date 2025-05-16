03fefab4905a11e89a261fd511b195bf2953c43b
Michael Davis <michaeltcdavis@gmail.com>
2025-05-16 13:03:50 -0400

    feature/ add create order logic and Flyway create order table migration

---
+++ src/main/java/com/springlooks/order_service/service/OrderService.java
@@ -0,0 +1,27 @@
+package com.springlooks.order_service.service;
+
+import com.springlooks.order_service.dto.OrderRequest;
+import com.springlooks.order_service.model.Order;
+import com.springlooks.order_service.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = Order.builder()
+                .orderNumber(UUID.randomUUID().toString())
+                .price(orderRequest.price())
+                .skuCode(orderRequest.skuCode())
+                .quantity(orderRequest.quantity())
+                .build();
+        orderRepository.save(order);
+    }
+}
