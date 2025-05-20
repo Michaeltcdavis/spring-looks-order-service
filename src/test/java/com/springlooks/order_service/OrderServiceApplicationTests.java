8bdf02d1bb8e6b42792712b2ee595a25f289f39e
Michael Davis <michaeltcdavis@gmail.com>
2025-05-20 12:18:39 -0400

    add integration test with testContainers, rest-assured

--- src/test/java/com/springlooks/order_service/OrderServiceApplicationTests.java
+++ src/test/java/com/springlooks/order_service/OrderServiceApplicationTests.java
@@ -1,15 +1,60 @@
 package com.springlooks.order_service;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
 
+	@LocalServerPort
+	private Integer port;
+
+	@ServiceConnection
+	static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.4.5");
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mysql.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldSubmitOrder() {
+		String orderBody = """
+				{
+					"skuCode": "white shoe",
+					"price": 110,
+					"quantity": 1
+				}
+				""";
+		var responseBodyString = RestAssured.given()
+				.contentType("application/json")
+				.body(orderBody)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.extract()
+				.body().asString();
+
+		assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
 	}
 
 }
