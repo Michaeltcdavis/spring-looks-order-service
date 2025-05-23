28f7ea6cc46c6b79166cbf80de0e8556b95e6bb3
Michael Davis <michaeltcdavis@gmail.com>
2025-05-23 13:50:05 -0400

    feature/ add inventory check for order creation with open feign

--- src/main/java/com/springlooks/order_service/service/OrderService.java
+++ src/main/java/com/springlooks/order_service/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.springlooks.order_service.service;
 
+import com.springlooks.order_service.client.InventoryClient;
 import com.springlooks.order_service.dto.OrderRequest;
 import com.springlooks.order_service.model.Order;
 import com.springlooks.order_service.repository.OrderRepository;
@@ -14,8 +15,14 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
+        boolean inStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (!inStock)
+            throw new RuntimeException("Product: '" + orderRequest.skuCode() + "' is not in stock for quantity: " + orderRequest.quantity());
+
         Order order = Order.builder()
                 .orderNumber(UUID.randomUUID().toString())
                 .price(orderRequest.price())
